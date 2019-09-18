e4d42b5d77b9aaffcee7c0637e42b7571765ee6b
SoyManucho <alvarez.jmanuel94@gmail.com>
2019-09-17 23:06:14 -0300

    client user

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -1,12 +1,14 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\User;
 
 $factory->define(App\Client::class, function (Faker $faker) {
   return [
     'name'=>$faker->name,
     'phone'=>$faker->tollFreePhoneNumber,
-    'address'=>$faker->streetAddress
+    'address'=>$faker->streetAddress,
+    'id_user'=> User::inRandomOrder()->first()
 
   ];
 });
